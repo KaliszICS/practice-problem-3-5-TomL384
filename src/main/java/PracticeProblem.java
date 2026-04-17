2da7b5a0d9ee4b422c947ac3dc6dea5f172e126b
TomL384 <350286454@tdsb.ca>
2026-04-17 15:28:36 +0000

    Final

--- src/main/java/PracticeProblem.java
+++ src/main/java/PracticeProblem.java
@@ -1,7 +1,14 @@
+/**
+ * File Name: Problem Solving and Good Programming Style
+ * Author: Tom Leung
+ * Date Created: April 17, 2026
+ * Date Last Edited: April 17, 2026
+ */
+
 public class PracticeProblem {
 
 	public static void main(String args[]) {
-
+		System.out.println(isValidName("J Doe"));
 	}
 
 	public static void q1() {
@@ -24,4 +31,29 @@ public class PracticeProblem {
 		//Write question 5 code here
 	}
 
+	public static String getFirstName (String s1){
+		String s2 = s1.trim() ;
+		String s3 = s2.substring(0,s2.indexOf(" "));
+		return s3 ;
+		}
+	
+	public static String getLastName (String s1){
+		String s2 = s1.trim() ;
+		String s3 = s2.substring(s2.lastIndexOf(" ")+1);
+		return s3 ;
+		}
+	
+
+	public static boolean isValidName (String s1){
+		if (s1.length() < 1 || !s1.contains(" ") ){
+			return false ;
+		}
+		String s2 = s1.trim() ;
+		String s3 = s2.substring(0,s2.indexOf(" "));
+		String s4 = s2.substring(s2.indexOf(" ")+1);
+		return ( s3.length() > 1 && s4.length() > 1 && s2.indexOf(" ") == s2.lastIndexOf(" ")) ;
+
+	}
+
 }
+
